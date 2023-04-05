32d8fbff5968e1b2be81fb82a4cf39ff4c5f1881
Louistam888 <louistam888@gmail.com>
2023-04-04 22:50:14 -0400

    conditionals added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
 
 def encrypt(plain_text, shift_amount):
   cipher_text=""
@@ -15,4 +15,17 @@ def encrypt(plain_text, shift_amount):
     
   print(f"The encoded text is {cipher_text}")
 
-encrypt(text, shift)
+def decrypt(cipher_text, shift_amount):
+  deciphered_text=""
+  for j in cipher_text:
+    position = alphabet.index(j)
+    new_position = position - shift_amount
+    new_letter = alphabet[new_position]
+    deciphered_text+= new_letter
+  
+  print(f"The deciphered text is {deciphered_text}")
+
+if direction == "encode":
+  encrypt(text, shift)
+elif direction == "decode":
+  decrypt(text, shift)
